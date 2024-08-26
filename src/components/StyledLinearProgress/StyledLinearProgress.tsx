a210d97804f0014a3c761edbff9845453e8b0839
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-08-26 18:34:36 +0300

    [SPT-575] updated StyledLinearProgress component

---
+++ src/components/StyledLinearProgress/StyledLinearProgress.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import LinearProgress from '@mui/material/LinearProgress';
+import { StyledLinearProgressProps } from './StyledLinearProgress.types';
+import { useStyles } from './styles';
+
+const StyledLinearProgress: FC<StyledLinearProgressProps> = ({ value }) => {
+    const classes = useStyles();
+    return (
+        <LinearProgress
+            className={classes.styledProgressBar}
+            variant='determinate'
+            value={value}
+        />
+    );
+};
+
+export default StyledLinearProgress;
---
+++ src/components/StyledLinearProgress/StyledLinearProgress.types.ts
@@ -0,0 +1,3 @@
+export interface StyledLinearProgressProps {
+    value: number;
+}
