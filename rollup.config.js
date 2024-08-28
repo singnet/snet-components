7f675430295611ee0ce6cad562c17e0f368997f9
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-08-28 15:58:58 +0300

    [SPT-551] updated rollup config for include styles

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,54 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import image from '@rollup/plugin-image';
-import dts from 'rollup-plugin-dts';
-import terser from '@rollup/plugin-terser';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import image from "@rollup/plugin-image";
+import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-const packageJson = require('./package.json');
+const packageJson = require("./package.json");
 
 export default [
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            peerDepsExternal(),
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: './tsconfig.json' }),
-            terser(),
-            image(),
-        ],
-        external: ['react', 'react-dom', 'styled-components'],
-    },
-    {
-        input: 'dist/esm/types/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'es' }],
-        plugins: [dts.default()],
-    },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+      image(),
+    ],
+    external: [
+      "react",
+      "react-dom",
+      "styled-components",
+      "react",
+      "react-dom",
+      "prop-types",
+      "@emotion/react",
+      "@emotion/styled",
+      "@mui/icons-material",
+      "@mui/material",
+      "@mui/material/styles",
+      "@mui/styles",
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts.default()],
+  },
 ];
