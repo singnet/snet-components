b91c64e8361ff44405b9c3e23151bbc2e84afe2b
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-08-29 17:15:21 +0300

    [SPT-571] removed unused files

--- src/utils/BlockChainError.js
+++
@@ -1,6 +0,0 @@
-export default class BlockChainError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "BlockChainError";
-    }
-}
--- src/utils/keyCodes.js
+++
@@ -1,3 +0,0 @@
-export const keyCodes = {
-    enter: 13,
-};
